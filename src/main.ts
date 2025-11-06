75bd737bbeab1c2e658c959f1d5e7f1d1b49ad61
aRubenDev <aruben.guerra@gmail.com>
2025-11-06 03:21:46 +0100

    [feat|minimal] Added custom message when route not found

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,21 @@ async function bootstrap() {
     }),
   )
 
+  app.enableCors(
+    {
+      origin: '*',
+      methods: 'GET,PATCH,POST,DELETE',
+    }
+  );
+
+  //agrego un middleware para dar una respuesta personalizada en caso de ruta no encontrada
+  app.use((req, res, next) => {
+    res.status(404).json({
+      statusCode: 404,
+      message: 'Resource not found',
+    });
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
